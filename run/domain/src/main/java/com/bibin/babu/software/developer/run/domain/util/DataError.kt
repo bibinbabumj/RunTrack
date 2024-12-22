1e216857b70622bf3ed3863ed291dabf967bb3c5
bibinbabumj <bibinbabumj93@gmail.com>
2024-12-22 01:43:39 +0000

    error-handling

---
+++ run/domain/src/main/java/com/bibin/babu/software/developer/run/domain/util/DataError.kt
@@ -0,0 +1,19 @@
+package com.bibin.babu.software.developer.run.domain.util
+
+sealed interface DataError : Error {
+    enum class Network : DataError {
+        REQUEST_TIMEOUT,
+        UNAUTHORIZED,
+        CONFLICT,
+        TOO_MANY_REQUESTS,
+        NO_INTERNET,
+        PAYLOAD_TOO_LARGE,
+        SERVER_ERROR,
+        SERIALIZATION,
+        UNKNOWN
+    }
+
+    enum class Local : DataError {
+        DISKS_FULL
+    }
+}
---
+++ run/domain/src/main/java/com/bibin/babu/software/developer/run/domain/util/Error.kt
@@ -0,0 +1,5 @@
+package com.bibin.babu.software.developer.run.domain.util
+
+interface Error {
+
+}
